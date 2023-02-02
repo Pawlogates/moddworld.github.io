3d82719dcf2ff9a4a394d270d94113d27bade7be
Pawlogates <99600289+Pawlogates@users.noreply.github.com>
2023-02-02 14:21:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 
+$(function(){
+    $('.divtop').on('click', function(){
+    $('.odd').toggleClass('odd_on');
+
+    });
+});
+
+$(function(){
+    $('.divtop').on('click', function(){
+    $('.divtop').toggleClass('odd_on2');
+
+    });
+});
+
+$(function(){
+    $('.divtop').on('click', function(){
+    $('.introbtn').toggleClass('btnflip');
+
+    });
+});
+
+$(function(){
+    $('.divtop').on('click', function(){
+    $('.presser1').toggleClass('p1alt');
+
+    });
+});
+
+$(document).scroll(function() {
+    var scroll = $(window).scrollTop();
+    $(".coolfollow").css("top", "0" + (scroll / 1.5) + "px");
+  });
+
+  $(window).on('load', function() {
+	$(".loader").delay(7000).hide();
+	$(".container").delay(2000).fadeIn(1);
+});
+
+
+var parallax = document.querySelectorAll(".parallax");
+var	speed = -0.25;
+
+window.onscroll = function() {
+	[].slice.call(parallax).forEach(function(el, i) {
+
+		var windowYOffset = window.pageYOffset,
+			elBackgrounPos = "50%" + (windowYOffset * speed + i * -100) + "px";
+
+		el.style.backgroundPosition = elBackgrounPos;
+
+	});
+};
+
+var rellax = new Rellax('.rellax');
+
+
+
+
